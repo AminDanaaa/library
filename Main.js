3cdf40afb8ff791aee495900565f8e4d64954403
Amin Dana <AminDanaaa@Gmail.com>
2026-08-28 19:02:24 +0330

    change ui to neumorph style and change color palette

--- Main.js
+++ Main.js
@@ -1,5 +1,5 @@
 // Imports and global constants
-const deleteBtnSVG = '<svg xmlns="http://www.w3.org/2000/svg" height="24px" viewBox="0 -960 960 960" width="24px" fill="red"><path d="M280-120q-33 0-56.5-23.5T200-200v-520h-40v-80h200v-40h240v40h200v80h-40v520q0 33-23.5 56.5T680-120H280Zm400-600H280v520h400v-520ZM360-280h80v-360h-80v360Zm160 0h80v-360h-80v360ZM280-720v520-520Z"/></svg>';
+const deleteBtnSVG = '<svg xmlns="http://www.w3.org/2000/svg" height="24px" viewBox="0 -960 960 960" width="24px"><path fill="#ff5a7a" d="M280-120q-33 0-56.5-23.5T200-200v-520h-40v-80h200v-40h240v40h200v80h-40v520q0 33-23.5 56.5T680-120H280Zm400-600H280v520h400v-520ZM360-280h80v-360h-80v360Zm160 0h80v-360h-80v360ZM280-720v520-520Z"/></svg>';
 const container = document.querySelector(".container");
 const myLibrary = [];
 
@@ -27,6 +27,7 @@ function addBookToLibrary(title, author, pages, isRead) {
 function createCard() {
     let card = document.createElement("div");
     card.classList.add("book-card");
+    card.classList.add("neumorph-card");
     container.appendChild(card);
     return card;
 }
